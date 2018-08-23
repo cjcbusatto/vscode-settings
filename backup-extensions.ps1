7cc1e24f85fb29b96f53fd5b7678f418f2c6d0ac
Claudio Busatto <c.busatto@wille-enginering.com>
2018-08-23 11:58:32 +0200

    Renamed the installed-extensions.ps1 to reinstall-extensions

--- backup-extensions.ps1
+++ backup-extensions.ps1
@@ -1,6 +1,6 @@
 $listExtensionsCmd = code --list-extensions
 
-"# Installed-Extensions on $(Get-Date)" > "installed-extensions.ps1"
+"# Installed-Extensions on $(Get-Date)" > "reinstall-extensions.ps1"
 Foreach ($extension in $listExtensionsCmd) {
-    "code --install-extension $extension" >> "installed-extensions.ps1"
+    "code --install-extension $extension" >> "reinstall-extensions.ps1"
 }
